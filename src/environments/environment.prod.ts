ab3691965882ff2206d5f053e041d1882b8ed529
Alan Roy Aupart Vargas <alan.aupart.vargas@kairosds.com>
2023-07-18 18:29:21 -0600

    Se añaden valiables de entorno

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  url_login: 'http://ec2-3-85-108-239.compute-1.amazonaws.com:8002/msvc-login/api/auth',
+  url_catalogo: 'http://ec2-3-85-108-239.compute-1.amazonaws.com:8003/msvc-catalogo/v1/api',
+};
